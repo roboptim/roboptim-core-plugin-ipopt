1954545ef046b2501c07ff73638caf889697d93c
Francois Keith <francois.keith@aist.go.jp>
2010-04-21 14:55:15 +0900

    Add missing file required to compile the sample tests.
        * msvc/roboptim-core-plugin-ipopt/config.h: Give details on this
          package (version, bugreport...).
    
    Signed-off-by: Keith Francois <francois.keith@aist.go.jp>

---
+++ msvc/roboptim-core-plugin-ipopt/config.h
@@ -0,0 +1,20 @@
+/* Name of package */
+#define PACKAGE "roboptim-core-ipopt-plugin"
+
+/* Define to the address where bug reports for this package should be sent. */
+#define PACKAGE_BUGREPORT "roboptim-user@lists.sourceforge.net"
+
+/* Define to the full name of this package. */
+#define PACKAGE_NAME "RobOptim core IPOPT plug-in"
+
+/* Define to the full name and version of this package. */
+#define PACKAGE_STRING "RobOptim core IPOPT plug-in 0.2"
+
+/* Define to the one symbol short name of this package. */
+#define PACKAGE_TARNAME "roboptim-core-ipopt-plugin"
+
+/* Define to the version of this package. */
+#define PACKAGE_VERSION "0.2"
+
+/* Version number of package */
+#define VERSION "0.2"
