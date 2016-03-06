22358599b224985c157cefaf820314fb8e617e45
JRL/IDH Continuous Integration Tool <jrl-idh+ci@gmail.com>
2016-03-06 09:39:08 +0000

    Update HEAD Doxygen documentation. Source commit id: c82643b2367b9e39fd0d34ad5d00fe5dd8ac744d

--- doxygen/HEAD/files.js
+++ doxygen/HEAD/files.js
@@ -1,15 +1,23 @@
 var files =
 [
     [ "doc.hh", "a00007.html", null ],
-    [ "ipopt-common.hh", "a00008.html", null ],
+    [ "ipopt-common.hh", "a00008.html", [
+      [ "SmartPtr", "a00005.html", null ]
+    ] ],
     [ "ipopt-common.hxx", "a00009.html", "a00009" ],
     [ "ipopt-parameters-updater.hh", "a00010.html", "a00010" ],
     [ "ipopt-sparse.cc", "a00011.html", "a00011" ],
-    [ "ipopt-sparse.hh", "a00012.html", null ],
+    [ "ipopt-sparse.hh", "a00012.html", [
+      [ "Tnlp", "a00006.html", "a00006" ]
+    ] ],
     [ "ipopt-td.cc", "a00013.html", "a00013" ],
-    [ "ipopt-td.hh", "a00014.html", null ],
+    [ "ipopt-td.hh", "a00014.html", [
+      [ "Tnlp", "a00006.html", "a00006" ]
+    ] ],
     [ "ipopt.cc", "a00015.html", "a00015" ],
-    [ "ipopt.hh", "a00016.html", null ],
+    [ "ipopt.hh", "a00016.html", [
+      [ "Tnlp", "a00006.html", "a00006" ]
+    ] ],
     [ "tnlp.cc", "a00017.html", null ],
     [ "tnlp.hh", "a00018.html", "a00018" ],
     [ "tnlp.hxx", "a00019.html", "a00019" ]
--- doxygen/HEAD/search/functions_75.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['userscaling',['userScaling',['../a00002.html#a4c447c392c704b0cb404cf12bfbd5561',1,'roboptim::IpoptSolverCommon']]]
-];
