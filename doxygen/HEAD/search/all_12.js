6ba182e13a119f4d31f0ec14a54dce6c9ef7611c
JRL/IDH Continuous Integration Tool <jrl-idh+ci@gmail.com>
2016-03-06 10:29:46 +0000

    Update HEAD Doxygen documentation. Source commit id: e3a4d46f0d170c07ad4ac7748b3b6f95a68b7325

--- doxygen/HEAD/search/all_12.js
+++
@@ -1,5 +0,0 @@
-var searchData=
-[
-  ['user_20manual',['User manual',['../index.html',1,'']]],
-  ['userscaling',['userScaling',['../a00002.html#a4c447c392c704b0cb404cf12bfbd5561',1,'roboptim::IpoptSolverCommon']]]
-];
